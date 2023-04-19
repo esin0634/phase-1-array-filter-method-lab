ae9bed53e30bdb65099cc748aa8e4f832561c036
Esin <nbusra.esin@gmail.com>
2023-04-19 15:10:21 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+
+
+function findMatching (drivers, query){
+   return drivers.filter(driver => driver.toLowerCase() === query.toLowerCase()
+   )
+}
+
+function fuzzyMatch(drivers,queryArray){
+    return drivers.filter(driver => queryArray.includes(driver[0]))
+}
+
+function matchName(drivers, query){
+    return drivers.filter(driver => driver.name === query)
+}
